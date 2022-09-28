88a2553b93cdbf50626a0117aa363f057ca143ef
shukriabotteen <sabotte@ilstu.edu>
2022-09-28 16:27:43 -0500

    Initial commit

---
+++ src/isu/edu/MainClass.java
@@ -0,0 +1,27 @@
+package isu.edu;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Queue<Integer> line= new LinkedList<Integer>();
+		line.offer(1);
+		line.offer(1);
+		line.offer(2);
+		Integer a= line.poll();
+		
+		line.offer(a);
+		if(line.poll()==a)
+			System.out.println("yes");
+		
+		
+		
+	}
+
+}
